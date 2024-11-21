4fb6e8c645c35c9b648edfa7890b4083455f501f
Alexis Huvier <lavapower84@gmail.com>
2024-11-21 22:38:46 +0100

    fix(SEAetherPhysics): fix versions

--- SharpEngine.AetherPhysics/SEAetherPhysics.cs
+++ SharpEngine.AetherPhysics/SEAetherPhysics.cs
@@ -28,7 +28,7 @@ public static class SEAetherPhysics
     /// </summary>
     public static void AddVersions()
     {
-        DebugManager.Versions.Add("Aether.Physics2D", "2.0.0");
-        DebugManager.Versions.Add("SharpEngine.AetherPhysics", "1.2.2");
+        DebugManager.Versions.Add("Aether.Physics2D", "2.1.0");
+        DebugManager.Versions.Add("SharpEngine.AetherPhysics", "1.3.1");
     }
 }
